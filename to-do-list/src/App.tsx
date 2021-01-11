6ff0a14fe4e3398926f62baa4ec93736b087f7da
Leoander Neves <leoanderneves8@outlook.com>
2021-01-11 17:51:28 -0300

    To do list

---
+++ to-do-list/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React Todo</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// https://medium.com/@nerdjfpb/15-react-js-project-ideas-beginner-to-expert-with-free-tutorial-55077e04198c
